2a94ecb7a959acc456830ed1473c0e36a6e0a2ab
Wawan <kurnia.setyo13@gmail.com>
2023-11-01 08:22:06 +0700

    update

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.status(200)
+        .json({
+            message: "it's working"
+        });
+});
+
+const port = 8000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
