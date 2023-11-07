91c09d805a9cc993522aeef4e6d5d305f463f05c
wawan-skyshi <110967066+wawan-skyshi@users.noreply.github.com>
2023-11-07 21:20:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     return res.status(200)
         .json({
-            message: "it's working now! for test"
+            message: "it's working now! for test untuk testing lagi"
         });
 });
 
